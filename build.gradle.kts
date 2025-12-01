e44a494ae97629f8ade3c0b6d27d6f1d988564ea
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2025-12-01 02:16:21 -0300

    Configure Java and Kotlin target compatibility to version 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,18 @@ application {
     mainClass.set("MainKt")
 }
 
+// Configure Java and Kotlin target compatibility
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
